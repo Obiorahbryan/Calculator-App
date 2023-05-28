58c3ab1c8046c6bf09aa793277680fc055eec1bf
Obiorah Bryan <106826157+Obiorahbryan@users.noreply.github.com>
2023-05-28 23:59:22 +0100

    progress

---
+++ script.js
@@ -0,0 +1,7 @@
+const 
+    keys = document.querySelectorAll('.key'),
+    input = document.querySelector('input');
+
+input.addEventListener('change', function () {
+    console.log(input.value);
+})
