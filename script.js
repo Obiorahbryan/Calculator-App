b94d2d073acbe5fc5ceb7a75fa03c16f8b7c47e7
Obiorah Bryan <106826157+Obiorahbryan@users.noreply.github.com>
2023-05-29 00:36:20 +0100

    progress report

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const 
-    keys = document.querySelectorAll('.key'),
-    input = document.querySelector('input');
+    numbers = document.querySelectorAll('.number'),
+    input = document.querySelector('input'),
+    clear = document.querySelector('.clear'),
+    deleteBtn = document.querySelector('.deleteBtn');
 
 input.addEventListener('change', function () {
-    console.log(input.value);
+    
+})
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        input.value += number.textContent;
+    })
+})
+
+clear.addEventListener('click', () => {
+    input.value = "";
+})
+
+deleteBtn.addEventListener('click', () => {
+    if (!(input.value.trim())) {
+        return;
+    } else {
+        const arr = input.value.split("");
+        arr.pop()
+        const str = arr.join("");
+        input.value = str;
+    }
 })
